e02c3f585d9ac84219ae4985626cd2ce9ec72143
alkisom <al.khisom47@guru.smk.belajar.id>
2024-09-13 17:06:38 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggle-mode').addEventListener('click', () => {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-
-    const isDarkMode = body.classList.contains('dark-mode');
-    document.getElementById('toggle-mode').textContent = isDarkMode ? 'Mode Siang' : 'Mode Malam';
-});
